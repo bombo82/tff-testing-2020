09f3699dde9fa2410dc886a9bd18e5a1266c71df
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2020-07-15 20:53:29 +0200

    StringCalculator initial commit

---
+++ StringCalculatorAltImpl/StringCalculatorTest/StringCalculatorService_Should.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace StringCalculator.Test
+{
+    public class StringCalculatorService_Should
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
