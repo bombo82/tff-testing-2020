2fdf12e1a17582e093676c1e4f13b5b599ff922f
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2020-07-17 08:48:15 +0200

    LeapYear initial commit

---
+++ LeapYear/LeapYearService.Test/LeapYearService_Should.cs
@@ -0,0 +1,21 @@
+using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+using LeapYear.Service;
+
+namespace LeapYear.UnitTest.Service
+{
+    public class LeapYearService_Should
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
