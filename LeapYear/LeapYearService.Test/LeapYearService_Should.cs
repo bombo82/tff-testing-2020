514f1d02a686a11720719bede95dbc915ed2f0b0
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2020-07-17 08:41:58 +0200

    leap year

--- LeapYear/LeapYearService.Test/LeapYearService_Should.cs
+++ LeapYear/LeapYearService.Test/LeapYearService_Should.cs
@@ -7,15 +7,57 @@ namespace LeapYear.UnitTest.Service
 {
     public class LeapYearService_Should
     {
+        private LeapYearService service;
+
         [SetUp]
         public void Setup()
         {
+            service = new LeapYearService();
         }
 
         [Test]
-        public void Test1()
+        public void ReturnTrue_WhenYearIsMultipleOfFour()
         {
-            Assert.Pass();
+            Assert.That(service.IsLeapYear(8), Is.True);
+        }
+
+        [Test]
+        public void ReturnFalse_WhenYearIsNotMultipleOfFour()
+        {
+            Assert.That(service.IsLeapYear(11), Is.False);
+        }
+
+        [Test]
+        public void ReturnFalse_WhenYearIsACenturyAfter1582()
+        {
+            Assert.That(service.IsLeapYear(1800), Is.False);
+        }
+
+        [Test]
+        public void ReturnTrue_WhenYearIsACenturyBefore1582()
+        {
+            Assert.That(service.IsLeapYear(1500), Is.True);
+        }
+
+        [Test]
+        public void ReturnTrue_WhenYearIsMultipleOf400()
+        {
+            Assert.That(service.IsLeapYear(2000), Is.True);
+        }
+
+        [TestCase(-4)]
+        [TestCase(-8)]
+        [TestCase(-1500)]
+        [TestCase(-2000)]
+        public void ReturnFalse_whenYearIsBefore0(int year)
+        {
+            Assert.That(service.IsLeapYear(year), Is.False);
+        }
+
+        [Test]
+        public void ThrowArgumentException_WhenYearIsZero()
+        {
+            Assert.That(() => service.IsLeapYear(0), Throws.ArgumentException);
         }
     }
 }
--- LeapYear/LeapYearService/LeapYearService.cs
+++ LeapYear/LeapYearService/LeapYearService.cs
@@ -4,5 +4,11 @@ namespace LeapYear.Service
 {
     public class LeapYearService
     {
+        public bool IsLeapYear(int year)
+        {
+            if (year == 0) throw new ArgumentException("Year zero (0) does NOT exists!");
+
+            return year > 0 && (year % 100 == 0 ? year % 400 == 0 || year < 1582 : year % 4 == 0);
+        }
     }
 }
