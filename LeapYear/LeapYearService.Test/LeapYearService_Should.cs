bafed5ed5ac2f078f468c51a3904ffe42e02535a
Gianni Bombelli (bombo82) <bombo82@giannibombelli.it>
2020-07-18 15:10:09 +0200

    LeapYear: add interface

--- LeapYear/LeapYearService.Test/LeapYearService_Should.cs
+++ LeapYear/LeapYearService.Test/LeapYearService_Should.cs
@@ -1,5 +1,3 @@
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
 using NUnit.Framework;
 using LeapYear.Service;
 
---
+++ LeapYear/LeapYearService/ILeapYearService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeapYear.Service
+{
+    /// <summary>
+    /// Given a year returns if it is a leap year.
+    /// </summary>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="year"> is <c>0</c></exception>
+    interface ILeapYearService
+    {
+        bool IsLeapYear(int year);
+    }
+}
